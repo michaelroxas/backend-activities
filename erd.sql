f471f605f290694895ad9303633565d1368e10e7
Michael Roxas <work.michaelroxas@gmail.com>
2021-09-27 23:47:53 +0800

    initial commit
    progress: 0/4

---
+++ erd.sql
@@ -0,0 +1,10 @@
+-- Display the number of unique inventory and the total number of inventory rented
+
+
+-- List all the films of Dan Torn and Dan Streep. Sort by `film_title` alphabetical order
+
+-- List all Comedy films of all actors whose last names start with `D`. Sort by `actor_full_name` alphabetical order
+
+-- Create the ERD of this database. 
+-- drawsql.app or
+-- https://app.diagrams.net/
