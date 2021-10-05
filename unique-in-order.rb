d539627663bbf1236ac77f75d2aa297eac052b64
Michael Roxas <work.michaelroxas@gmail.com>
2021-10-05 21:42:14 +0800

    Add instructions to code exercise
    source: codewars

--- unique-in-order.rb
+++ unique-in-order.rb
@@ -1,3 +1,8 @@
+# Instructions
+# unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+# unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+# unique_in_order([1,2,2,3,3])       == [1,2,3]
+
 def unique_in_order(iterable) 
 #  create an empty array
 content = []
